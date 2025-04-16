17d70687b6acd6d290ca27b25f9a2e69d22d5d30
BEYUD <beyud@attcloud.org>
2025-04-16 16:39:07 +0700

    maintainance

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,42 @@ import MissionHistory from "./pages/account/mission-history/Index";
 import MyCode from "./pages/account/my-code/Index";
 import ReceiveWord from "./pages/account/receive-word/Index";
 import SendWord from "./pages/account/send-word/Index";
+import { Flex, Image, Typography } from "antd";
+import MainImg from "./assets/main-img-1.png";
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Main />} />
+          {/* <Route path="/" element={<Main />} />
           <Route path="/account" element={<Account />}>
             <Route path="mission-history" element={<MissionHistory />} />
             <Route path="my-code" element={<MyCode />} />
             <Route path="receive-word" element={<ReceiveWord />} />
             <Route path="send-word" element={<SendWord />} />
-          </Route>
+          </Route> */}
         </Route>
+        <Route
+          path="*"
+          element={
+            <Flex
+              vertical
+              justify="center"
+              align="center"
+              className="bg-[var(--header-bg)] h-screen"
+              gap={50}
+            >
+              <Image src={MainImg} alt="main image" />
+              <Typography.Title
+                level={1}
+                className="!text-[var(--color-brand-primary)]"
+              >
+                Hệ thống đang bảo trì - vui lòng đợi đôi chút !!!
+              </Typography.Title>
+            </Flex>
+          }
+        />
       </Routes>
 
       <ToastContainer
