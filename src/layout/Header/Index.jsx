dc6309140b39829714069c79115f4639cdb42102
BEYUD <beyud@attcloud.org>
2025-05-02 08:06:19 +0700

    update header for new UI

--- src/layout/Header/Index.jsx
+++ src/layout/Header/Index.jsx
@@ -1,5 +1,6 @@
 import { Flex, Image } from "antd";
 import Logo from "../../assets/logo.png";
+import HomeIcon from "../../assets/home-icon.png";
 import LoginModal from "../../components/LoginModal";
 import UserImg from "../../assets/user-img.png";
 import SignUpBtn from "../../components/SignUpBtn";
@@ -13,11 +14,9 @@ export default function MainHeader({ Layout, ...rest }) {
   const navigate = useNavigate();
 
   return (
-    <Header
-      {...rest}
-      className="flex items-center md:justify-around justify-between !h-[86px]"
-    >
-      <div className="pt-3">
+    <Header {...rest} className="flex items-center justify-between !h-[86px]">
+      <Flex className="pt-3 md:gap-2.5 gap-1" justify="center" align="end">
+        <Image src={HomeIcon} preview={false} className="md:hidden block" />
         <Image
           src={Logo}
           alt="logo"
@@ -25,10 +24,13 @@ export default function MainHeader({ Layout, ...rest }) {
           className="md:!w-[138px] md:!h-[66px] !w-[73px] !h-[34px]"
           onClick={() => navigate("/")}
         />
-      </div>
+        <p className="text-white font-semibold text-xl border-b-[1.5px] border-[#FB8C00] pb-2 md:block hidden">
+          Trang chủ
+        </p>
+      </Flex>
       <Flex vertical justify="center" align="center">
         {isAuthenticated ? (
-          <Flex justify="center" align="center" gap={30}>
+          <Flex justify="center" align="center" className="md:gap-7 gap-3">
             <CustomButton
               label="Tài khoản"
               className={`md:px-7 py-1 md:!text-2xl !text-[12px] lg:h-[52px] md:h-[35px] h-[25px] p-3 flex justify-center items-center whitespace-nowrap`}
@@ -36,14 +38,19 @@ export default function MainHeader({ Layout, ...rest }) {
             />
             <Flex justify="center" align="center" gap={10}>
               <Flex vertical gap={5}>
-                <p className="text-[var(--orange-shade)] md:text-lg text-sm font-semibold">
+                <p className="text-[var(--orange-shade)] md:text-lg text-xs font-semibold">
                   Xin chào,
                 </p>
-                <p className="text-white md:text-2xl text-lg font-semibold">
+                <p className="text-white md:text-2xl text-sm font-semibold">
                   {localStorage.getItem("username")}
                 </p>
               </Flex>
-              <Image src={UserImg} alt="logo" preview={false} />
+              <Image
+                src={UserImg}
+                alt="logo"
+                preview={false}
+                className="md:!w-[65px] md:!h-[65px] !w-[40px] !h-[40px]"
+              />
             </Flex>
           </Flex>
         ) : (
