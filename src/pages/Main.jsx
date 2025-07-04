52f5c14f287107c9f63ad02ad0dcef65c26d1cdb
BEYUD <beyud@attcloud.org>
2025-07-04 14:28:40 +0700

    build 04/07

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,10 @@
 import Mobile from "./Mobile";
 import PC from "./PC";
 import { Flex, Image } from "antd";
-import PC1 from "../assets/pc1.png";
+// import PC1 from "../assets/pc1.png";
 import PC2 from "../assets/pc2.png";
 import PC3 from "../assets/pc3.png";
-import MB1 from "../assets/mb1.png";
+// import MB1 from "../assets/mb1.png";
 import MB2 from "../assets/mb2.png";
 import MB3 from "../assets/mb3.png";
 import TopTable from "../components/TopTable";
@@ -31,16 +31,16 @@ export default function Main() {
           align="center"
           className="md:flex md:visible md:opacity-100 md:h-auto md:w-auto h-0 w-0 hidden invisible opacity-0 lg:!px-6 !px-4 xl:gap-20 gap-5"
         >
-          <Flex justify="center" align="center" gap={40}>
+          {/* <Flex justify="center" align="center">
             <Image
               src={PC1}
               preview={false}
               alt="event-detail"
               loading="lazy"
             />
-          </Flex>
+          </Flex> */}
 
-          <Flex justify="center" align="center" gap={40}>
+          <Flex justify="center" align="center">
             <Image
               src={PC2}
               preview={false}
@@ -66,9 +66,9 @@ export default function Main() {
         className="md:hidden md:invisible md:opacity-0 md:h-0 md:w-0 flex visible opacity-100 !px-2 !mb-6"
         gap={30}
       >
-        <Flex justify="center" align="center">
+        {/* <Flex justify="center" align="center">
           <Image src={MB1} preview={false} alt="event-detail" loading="lazy" />
-        </Flex>
+        </Flex> */}
 
         <Flex justify="center" align="center">
           <Image
